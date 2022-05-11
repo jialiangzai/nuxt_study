bd32d689134aa24d907333a71c611bb4cb02c446
zuishuainanhai <1705902650@qq.com>
2022-05-11 20:33:43 +0800

    优化

--- middleware/each.js
+++ middleware/each.js
@@ -1,9 +1,11 @@
 export default (app) => {
   // console.log('中间件路由守卫上下文', app)
+
+  let token = app.$cookies.get('token')
+  console.log('token', token)
+
+  app.store.commit('setToken', token)
   if (!token) {
     app.redirect('/login')
-  } else {
-    let token = app.$cookies.get('token')
-    app.store.commit('setToken', token)
   }
 }
